1ee9130c595d82b2a09d3f0326527b6805f28136
Bas Huisman <bash@lostlemon.nl>
2024-03-06 15:12:32 +0100

    add background

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export default function Home() {
   return (
     <main>
-      <img
-        src="/logo.jpg"
-        alt="logo"
-        style={{
-          maxWidth: "100%",
-          maxHeight: "100%",
-          display: "block",
-          width: "auto",
-          height: "auto",
-        }}
-      />
+      <div className="container">
+        <img src="/logo.jpg" alt="logo" />
+      </div>
     </main>
   );
 }
