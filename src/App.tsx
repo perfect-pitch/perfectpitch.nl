fdc7e70e076a02e921b9a4c0b39f140c19b04ad8
Bas Huisman <bhuism@odee.net>
2025-03-03 09:49:58 +0100

    move to vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from "../public/logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="container">
+          <img src={logo} />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
